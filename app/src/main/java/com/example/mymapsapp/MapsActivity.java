6aa687f7c12551839798ae69bfc85b2150c14bac
Elizabeth Kwon <elizabeth.h.kwon@gmail.com>
2019-06-04 11:09:26 -0700

    google maps updated

--- app/src/main/java/com/example/mymapsapp/MapsActivity.java
+++ app/src/main/java/com/example/mymapsapp/MapsActivity.java
@@ -1,5 +1,8 @@
 package com.example.mymapsapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -39,8 +42,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng korea = new LatLng(37.5665, 126.9780);
+        mMap.addMarker(new MarkerOptions().position(korea).title("Place of birth: Seoul"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(korea));
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
     }
 }
